acd74acf03e47f35d2d4c5850b276c5b9490a414
Jerry Yang <jerry871002@gmail.com>
2022-11-19 18:06:54 +0200

    update grader service

--- grader/app.js
+++ grader/app.js
@@ -1,40 +1,8 @@
 import { connect } from './deps.js';
 import { grade } from './grade.js';
 
-const notifyApiServer = (result) => {
-
-}
-
-// const connectRabbitMQ = () => {
-//   amqp.connect('amqp://rabbitmq', function (error0, connection) {
-//     if (error0) {
-//       throw error0;
-//     }
-//     connection.createChannel(function (error1, channel) {
-//       if (error1) {
-//         throw error1;
-//       }
-//       var queue = 'submissions';
-
-//       channel.assertQueue(queue, {
-//         durable: false
-//       });
-
-//       console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-//       channel.consume(queue, function (msg) {
-//         console.log(" [x] Received %s", msg.content.toString());
-//         console.log(" [x] Grading the exercise...");
-//         const result = grade(msg.content);
-//         notifyApiServer(result); // TODO add actual logic inside
-//       }, {
-//         noAck: true
-//       });
-//     });
-//   });
-// }
 
 const connectRabbitMQ = async () => {
-
   const connection = await connect({
     hostname: 'rabbitmq'
   });
@@ -48,9 +16,22 @@ const connectRabbitMQ = async () => {
     async (args, props, data) => {
       const submission = JSON.parse(new TextDecoder().decode(data));
       console.log(" [x] Received %s", submission);
+
       console.log(" [x] Grading the exercise...");
       const result = await grade(submission.code);
       console.log(" [x] The result of this submission is %s", result);
+
+      console.log(" [x] Notify API server the result");
+      const response = await fetch('http://web:7800/api/result', {
+        method: 'POST',
+        body: JSON.stringify({
+          ...submission,
+          result: result
+        })
+      });
+      const responseJSON = await response.json();
+      console.log(responseJSON);
+
       await channel.ack({ deliveryTag: args.deliveryTag });
     },
   );
